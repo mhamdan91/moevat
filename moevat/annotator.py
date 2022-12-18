ef3c08690eca16eaf0f2aac5b6ea0173a5acf1b1
Muhammad Hamdan <muhammad.hamdan@intel.com>
2022-12-18 01:15:54 -0800

    All components added and tool is ready

--- moevat/annotator.py
+++ moevat/annotator.py
@@ -1,30 +1,46 @@
 import glob, csv, os, typing
+import logging
+import shutil
+import json
 import cv2
 import numpy as np
-import logging
+from pathlib import Path
+from moethread import parallel_call
 
 logger = logging.getLogger(__name__)
-logger_format='[%(asctime)s | %(name)s | LN%(lineno)s | %(levelname)s]: %(message)s'
-logging.basicConfig(level=logging.INFO, format=logger_format)
 
-def resize_img(image, x=850, y=640):
+def resize_img(image, percentage):
     shape = image.shape
-    _y = shape[0]
-    _x = shape[1]
-    if _y < y:
-        x = _x
-        y = _y
+    y = int(shape[0] * percentage / 100)
+    x = int(shape[1] * percentage / 100)
     return cv2.resize(image, (x, y), interpolation=cv2.INTER_AREA)
 
-def write_results(output_path: str, labels_dict: typing.Dict):
-    header = ['image_name', 'label', 'class']
-    with open(output_path, newline='', mode='w') as of:
-        writer = csv.DictWriter(of, fieldnames=header)
-        writer.writeheader()
-        for key, value in labels_dict.items():
-            writer.writerow(value)
+def write_results(output_name: str, labels_dict: typing.Dict):
+    if os.path.splitext(output_name)[-1].lower() == '.csv':
+        header = ['image_name', 'label', 'class']
+        with open(output_name, newline='', mode='w') as of:
+            writer = csv.DictWriter(of, fieldnames=header)
+            writer.writeheader()
+            for value in labels_dict.values():
+                writer.writerow(value)
+    else:
+        with open(output_name, mode='w') as of:
+            json.dump(labels_dict, of, separators=[',', ':'], indent=4)
 
-def annotate(images_path: str, output_path: str, config: typing.Dict, tooltip: bool=False, loop: bool=True):
+def load_existing_labels(output_name: str) -> typing.Dict:
+    existing_labels = {}
+    if os.path.isfile(output_name):
+        with open(output_name) as f:
+            existing_labels = list(csv.DictReader(f))
+    existing_labels_dict = {}
+    if existing_labels:
+        for _dict in existing_labels:
+            image_name = os.path.splitext(_dict.get('image_name'))[0]
+            existing_labels_dict[image_name] = _dict
+    return existing_labels_dict
+
+def annotate(images_path: str, output_name: str, classes: typing.Any, data_transfer: str,
+             dst_folder: str, window_size: int, show_class_names: bool=True, loop: bool=True):
     """
         https://docs.opencv.org/4.x/d4/da8/group__imgcodecs.html
 
@@ -41,6 +57,28 @@ def annotate(images_path: str, output_path: str, config: typing.Dict, tooltip: b
         Radiance HDR - .hdr, .pic (always supported)
 
     """
+    window_width = 130
+    tooltip_string = ""
+    if classes and show_class_names:
+        tmp = []
+        for key, value in classes.items():
+            tmp.append(f"{value.upper()}: {key}")
+        tooltip_string = ' | '.join(tmp)
+    if len(tooltip_string) > window_width:
+        rows = np.ceil(len(tooltip_string) / window_width)
+        break_point = int(len(classes) / rows)
+        tmp = []
+        tooltip_strings =[]
+        for i, (key, value) in enumerate(classes.items()):
+            if i % break_point == 0 and i != 0:
+                tooltip_strings.append(' | '.join(tmp))
+                tmp = []
+            tmp.append(f"{value.upper()}: {key}")
+        tooltip_strings.append(' | '.join(tmp))
+    else:
+        tooltip_strings = [tooltip_string]
+    if len(tooltip_strings) > 3:
+        logger.warning("You have very long class names, this is not recommened. Tooltip will look confusing.")
     supported_formats       = ['.bmp', '.dib', '.jpg', '.jpeg', '.jpe', '.jp2', '.png', '.webp', '.pmb', '.pmg',
                                '.ppm', '.pxm', '.pnm', '.pfm', '.sr', '.ras', '.tiff', '.tif', '.exr', '.hdr', '.pic']
     font                    = cv2.FONT_HERSHEY_SIMPLEX
@@ -48,44 +86,40 @@ def annotate(images_path: str, output_path: str, config: typing.Dict, tooltip: b
     fontColor               = (0,100,0)
     thickness               = 2
     lineType                = 1
-    
-    classes = {"0": "unknown", "1": "fm_strings", "2": "fm_blob", "3": "fm_spots", "4": "burn", "5": "ring", "6": "rust_spots", "7": "big_metal", "8": "nvd"}
-    existing_labels = {}
-    if os.path.isfile(output_path):
-        with open(output_path) as f:
-            existing_labels = list(csv.DictReader(f))
-
-    existing_labels_dict = {}
-    if existing_labels:
-        for _dict in existing_labels:
-            image_name = os.path.splitext(_dict.get('image_name'))[0]
-            existing_labels_dict[image_name] = _dict
-
-    items = glob.glob(images_path, recursive=True)
+    existing_labels_dict = load_existing_labels(output_name)
+    items = glob.glob(os.path.join(images_path, "**"), recursive=True)
     # Filter items based on existing labeled images and supported formats...
     items = [item for item in items if os.path.splitext(item.split(os.sep)[-1])[0] not in existing_labels_dict and \
              os.path.splitext(item.split(os.sep)[-1])[1].lower() in supported_formats]
-    num_items   = len(items)
+    if not items:
+        logger.warning("No items to label. If you wish to relabel, then delete the labels file in path. Early termination")
+        return
     forward     = 0
     labels_dict = {}
+    num_items   = len(items)
+    # Description area size...
+    dsize = int(50 * (75 + 25 * len(tooltip_strings)) / 90) if show_class_names else 40
     while forward < num_items:
         image_path  = items[forward]
         img         = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
         path_list   = image_path.split(os.sep)
         full_image_name  = path_list[-1]
-        image_name       = os.path.splitext(full_image_name)[0]
-        if tooltip:
-            description_area = img[:30, :]
-            description_area[:,:, 0]  = 255
-            description_area[:,:, 1]  = 255
-            description_area[:,:, 2]  = 255
-            img = np.vstack((description_area, img))
-            cv2.putText(img, "UNKNOWN: 0 | FM_STRINGS: 1 | FM_BLOB: 2 | FM_SPOTS: 3 | BURN: 4 | RING: 5 | RUST_SPOTS: 6 | BIG_METAL: 7 | NVD: 8", 
-                        (7, 20), font, 0.6, (180,0,0), thickness, lineType)
-            cv2.putText(img, "NEXT: RIGHT/UP ARROW | PREVIOUS: LEFT/DOWN ARROW", (7, 50), font, fontScale, (0,180,0), thickness, lineType)
-
-        title = f"current_image: {forward+1} | out of {num_items} || Click Escape to terminate labeling session."
-        cv2.imshow(title, resize_img(img))
+        description_area = img[:dsize, :]
+        description_area[:,:, 0] = 245
+        description_area[:,:, 1] = 245
+        description_area[:,:, 2] = 245
+        img = np.vstack((description_area, img))
+        cv2.putText(img, f"CURRENT ITEM: {forward + 1} | OUT OF {num_items} || CLICK ESACPE TO TERMINATE LABELING SESSION",
+                    (7, 30), font, fontScale, (0, 0, 180), thickness, lineType)
+        cv2.putText(img, "NEXT: RIGHT/UP ARROW | PREVIOUS: LEFT/DOWN ARROW", (7, 60), font, fontScale, (0, 180, 0), thickness, lineType)
+        if show_class_names:
+            for i, tooltip_string in enumerate(tooltip_strings):
+                red = min(255, 75*i)
+                green = min(25*i, 255)
+                cv2.putText(img, tooltip_string, (7, 100 + 25*i), font, 0.6, (150, green, red), thickness, lineType)
+        # Show GUI...
+        title = f"Moevat"
+        cv2.imshow(title, resize_img(img, window_size))
         cv2.moveWindow(title, 250, 96)
         key = cv2.waitKeyEx(0)
         label = -1
@@ -104,15 +138,21 @@ def annotate(images_path: str, output_path: str, config: typing.Dict, tooltip: b
             label = key - 48
             forward += 1
         else:
-            logger.warning("Invalid keystroke...")
+            logger.warning("Invalid keystroke.")
             continue
         if loop:
             forward = forward % num_items
         cv2.destroyAllWindows()
 
         if label > -1:
-            labels_dict[image_name] = {"image_name": full_image_name, "label": label, "class": classes[str(label)]}
-            logger.info(f" Labeled: {len(labels_dict)} out of {num_items} | {labels_dict[image_name]}")
+            klass = classes[label] if classes else label
+            labels_dict[image_path] = {"image_name": full_image_name, "label": label, "class": klass}
+            logger.info(f" Labeled: {len(labels_dict)} out of {num_items} | {labels_dict[image_path]}")
+            # Cache labeled data...
+            tmp = {}
+            tmp.update(existing_labels_dict)
+            tmp.update(labels_dict)
+            write_results(output_name, tmp)
         if len(labels_dict) == num_items:
             message = np.ones((50, 900, 3))
             title = "THANK YOU! Labeling is complete, program will exit shortly..."
@@ -121,9 +161,39 @@ def annotate(images_path: str, output_path: str, config: typing.Dict, tooltip: b
             cv2.moveWindow("THANK YOU", 350, 300)
             cv2.waitKey(2000)
             break
-
-    logger.info("Writing data to disk...")
-    labels_dict.update(existing_labels_dict)
-    write_results(output_path, labels_dict)
+    
     cv2.destroyAllWindows()
+    new_labeled_data = labels_dict.copy()
+    logger.info("Writing data to file...")
+    labels_dict.update(existing_labels_dict)
+    write_results(output_name, labels_dict)
+    if data_transfer in ['mv', 'cp']:
+        if not os.path.isdir(dst_folder):
+            _dst_folder = Path(dst_folder)
+            _dst_folder.mkdir(parents=True, exist_ok=True)
+
+        logger.info(f"Data/images will be transfered to: {dst_folder}")
+        if data_transfer == 'mv':
+            # Moving images from source directory to destination directory
+            command = shutil.move
+        else:
+            # Copying images from source directory to destination directory
+            command = shutil.copyfile
+
+        @parallel_call
+        def transfer_data(**kwargs):
+            image_path = kwargs.get('data', {}).get('image_path', '')
+            image_name = image_path.split(os.sep)[-1]
+            class_label = kwargs.get('data', {}).get('class_label', '')
+            dist_path = Path(os.path.join(dst_folder, class_label))
+            dist_path.mkdir(parents=True, exist_ok=True)
+            command(image_path, os.path.join(dist_path, image_name))
+        paths = []
+        class_labels = []
+        for key, value in new_labeled_data.items():
+            paths.append(key)
+            class_labels.append(value.get('class', ''))
+        action = 'Moving' if command == shutil.move else 'Copying'
+        logger.info(f"{action} data from source directory to destination directory...")
+        transfer_data(data={'image_path': paths, 'class_label': class_labels}, threads=-1)
 
--- moevat/cli.py
+++ moevat/cli.py
@@ -1,13 +1,46 @@
+# @mhamdan
+# MIT License
+
+# Copyright (c) 2022 Muhammad Hamdan
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import click
 import typing
 import yaml
 import os
+import logging
 from pathlib import Path
+from moevat.annotator import annotate
+import ctypes
+
+
 
 CONTEXT_SETTINGS    = dict(help_option_names=['-h', '--help'], max_content_width=150)
 FILE_TYPE           = click.Path(exists=True, dir_okay=False, resolve_path=True)
 DIRECTORY_TYPE      = click.Path(exists=True, file_okay=False, resolve_path=True)
 
+logger = logging.getLogger(__name__)
+# logger_format='[%(asctime)s | %(name)s | LN%(lineno)s | %(levelname)s]: %(message)s'
+logger_format='[%(asctime)s | MOEVAT | %(levelname)s]: %(message)s'
+logging.basicConfig(level=logging.INFO, format=logger_format)
 
 # Control help message...
 def command_required_option_from_option(require_name, require_map):
@@ -38,9 +71,9 @@ class RequiredIf(click.Option):
         super(RequiredIf, self).__init__(*args, **kwargs)
 
     def handle_parse_result(self, ctx, opts, args):
-        not_present = self.name not in opts
-        dependency_present = self.required_if in opts
-        if dependency_present and not not_present:
+        present = self.name in opts
+        dependency_not_present = self.required_if not in opts
+        if dependency_not_present and present:
             raise click.UsageError(
                 "Invalid usage: `%s` is required with `%s`" % (
                     self.name, self.required_if))
@@ -48,50 +81,162 @@ class RequiredIf(click.Option):
             self.prompt = None
         return super(RequiredIf, self).handle_parse_result(ctx, opts, args)
 
+# Dependable options...
+class NotRequiredIf(click.Option):
+    # https://stackoverflow.com/questions/44247099/click-command-line-interfaces-make-options-required-if-other-optional-option-is
+    def __init__(self, *args, **kwargs):
+        self.not_required_if = kwargs.pop('not_required_if')
+        assert self.not_required_if, "'not_required_if' parameter required"
+        kwargs['help'] = (kwargs.get('help', '') + ' NOTE: This argument is required with').strip()
+        super(NotRequiredIf, self).__init__(*args, **kwargs)
 
-@click.command(short_help="Command-line interface to label images. Refer to https://github.com/mhamdan91/moevat",
+    def handle_parse_result(self, ctx, opts, args):
+        not_present = self.name not in opts
+        exclusive_option_present = self.not_required_if in opts
+        if not exclusive_option_present and not_present:
+            raise click.UsageError(
+                "Invalid usage: `%s` is required" % (self.name))
+        else:
+            self.prompt = None
+        return super(NotRequiredIf, self).handle_parse_result(ctx, opts, args)
+
+
+@click.command(short_help="Command-line interface to label images. " \
+                          "Refer to https://github.com/mhamdan91/moevat",
              context_settings=CONTEXT_SETTINGS)
-@click.option('--images-path',  '-i',   required=True,
-                                        type=DIRECTORY_TYPE,
+@click.option('--images-path',  '-i',   type=DIRECTORY_TYPE,
+                                        cls=NotRequiredIf,
+                                        not_required_if='show_usage',
                                         help="Directory containing images.")
 @click.option('--output-name',  '-o',   type=click.Path(exists=False, dir_okay=False, resolve_path=False),
-                                        default="labels.csv",
+                                        cls=NotRequiredIf,
+                                        not_required_if='show_usage',
                                         show_default=True,
-                                        help="(optional) Output csv filename to store labels.",)
-@click.option('--labels-path',  '-l',   required=True,
-                                        type=click.Path(exists=True, dir_okay=False, resolve_path=True),
-                                        help="Path to yaml labels file. See example: https://github.com/mhamdan91/moevat")
-@click.option('--data-transfer','-d',   type=click.Choice(['cp', 'mv'], case_sensitive=False),
+                                        help="Output file path where labels will be to stored. (supported file formats are [csv, json])")                                 
+@click.option('--labels-path',  '-l',   type=click.Path(exists=True, dir_okay=False, resolve_path=True),
+                                        help="(optional) Required only if you wish to provide human readable classes to your data. " \
+                                             "See example labels yaml file: https://github.com/mhamdan91/moevat")
+@click.option('--dst-folder',   '-d',   type=click.Path(exists=False, file_okay=False, resolve_path=False),
+                                        help="(optional) Destination folder for labeled images.")
+@click.option('--data-transfer','-t',   type=click.Choice(['cp', 'mv', 'none'], case_sensitive=False),
                                         cls=RequiredIf,
                                         required_if='dst_folder',
-                                        default="NONE",
+                                        default="none",
                                         show_default=True,
                                         help="(optional) Copy (cp) or move (mv) data from source to destination folder " \
                                              "after completing labeling.")
-@click.option('--dst-folder',   '-f',   type=DIRECTORY_TYPE,
-                                        default="labeled_data",
-                                        show_default=True,
-                                        help="(optional) Destination folder for labeled images.")
 @click.option('--window-size',  '-w',   type=int,
-                                        default=800,
+                                        default=60,
                                         show_default=True,
-                                        help="(optional) Annotation window size (integer between 200-2000) to display images.")
-@click.option('--show-tooltip', '-t',   is_flag=True,
-                                        help="(optional) Flag to show tooltip while annotating.")
-def cli(images_path: DIRECTORY_TYPE, output_name: str, labels_path: str, data_transfer: bool,
-        dst_folder, window_size, show_tooltip, *args: typing.Any, **kwargs: typing.Any) -> None:
-    
-    if not os.path.isdir(dst_folder):
-        dst_folder = Path(dst_folder)
-        dst_folder.mkdir(parents=True, exist_ok=True)
+                                        help="(optional) Percentage (integer greater than zero) of original image size. " \
+                                             "Default display 60% of the original image size.")
+@click.option('--hide-labels',  '-x',   is_flag=True,
+                                        help="(optional) Flag to hide class/label names while annotating.")
+@click.option('--no-loop',      '-n',   is_flag=True,
+                                        help="(optional) Flag to stop looping over the dataset. " \
+                                             "By default user can navigate forward and backward, "
+                                             "e.g. start from left to right or right to left.")
+@click.option('--show-usage',   '-u',   is_flag=True,
+                                        help="(optional) Show detailed usage of the tool with examples and exit.")
+def cli(images_path: str, output_name: str, labels_path: str, data_transfer: bool,
+        dst_folder, window_size, hide_labels, no_loop: str, show_usage: bool, *args: typing.Any, **kwargs: typing.Any) -> None:
+    if show_usage:
+        logger.info(
+        """ 
+This tool allows you to quickly label images of up to 10 classes and this is basically because we 
+only have 10 numbers in NumPad :(O). If you have more than 10 classes, you can choose one class 
+to be all others and reclassfiy others afterwards. This tool is meant for quick labeling and is
+not meant to be an extensive and manually controlled GUI. 
 
+To use this tool you need to provide:
+- a directory containing data/images to label.
+- an output file path to store labeled data/images in and this file can only be of type [json, csv]
+- If you wish to copy or move labeled data/images after completing labeling, you must specify the 
+  `data-transfer` option where (cp -> copy, and mv -> move). You also need to specify a 
+  destination folder to transfer data to, this would be specifying the `dst-folder` option.
+- If you wish to resize window size that displays image and labeling instructions, you can 
+  provide an integer value that is greater than 0. This value will translate into a percentage, 
+  e.g [60] == 60% of the original image size and [200] == 200% of the original image size.
+- By default the tool will display the class names along with their human readable labels if
+  you provide a labels.yaml file. This file contains classes and human readable labels in 
+  the following format: (you can download this example from: https://github.com/mhamdan91/moevat)
     
-    from annotator import annotate
+    # ** Good class names **
+    classes:
+        0: "dog"
+        1: "cat"
+        2: "horse"
+        3: "mouse"
+        4: "rabbit"
+        5: "bird"
+        6: "car"
+        7: "human"
+        8: "elephant"
+        9: "house"
+
+    # ** Okay class names **
+    classes:
+        0: "brown dog"
+        1: "small cat"
+        2: "original horse"
+        3: "black mouse"
+        4: "white rabbit"
+        5: "big bird"
+        6: "red car"
+        7: "tall human"
+        8: "tiny elephant"
+        9: "huge house"
+
+    # ** Bad class names **
+    classes:
+        0: "dog is barking at the mailman"
+        1: "cat is sleeping deeply"
+        2: "horse is racing very fast"
+        3: "mouse is ticking extremely fast"
+        4: "rabbit is jumping around"
+        5: "bird is flying"
+        6: "car is very ice"
+        7: "human is playing well"
+        8: "elephant is too huge to move"
+        9: "house without windows"
+
+- The tool will allow you navigate forward and backward, by default the tool allows you loop through
+  images as long as you not labeled them all. This means you can start from left -> right or 
+  right -> left, i.e. from last-image --> first-image or from first-image -> last-image.
+- The tool will automatically cache data while you are labeling, and if you wish to end your labeling
+  session, simply click on ESCAPE.
+- If you wish to resume labeling from where you stopped last time, simply provide the labels file which
+  you used in the previous session and the tool will only show images that have not been labeled yet.
+        """)
+        return
+    
+    logger.info("MAKE SURE NumLock is ON...")
+    loop = False if no_loop else True
+    show_class_names = False if hide_labels else True
+    
+    tmp = list(os.path.splitext(output_name))
+    if tmp[-1].lower() not in ['.csv', '.json']:
+        logger.warning(f'Unsupported file format [{tmp[-1]}]. Defaulting to [csv]. Supported formats are [csv, json].')
+        tmp[-1] = '.csv'
+    output_dir = Path(f'{os.sep}'.join(tmp[0].split(os.sep)[:-1]))
+    output_dir.mkdir(parents=True, exist_ok=True)
+    output_name = f''.join(tmp)
+
+    if window_size < 1:
+        logger.warning(f"Received improper window size [{window_size}], setting to default: [60] (%).")
+        window_size = 60
     classes = {}
-    with open(labels_path) as f:
-        classes = yaml.safe_load(f)
-    annotate(images_path, output_name, classes, data_transfer, dst_folder, window_size, show_tooltip)
-
+    if labels_path:
+        try:
+            with open(labels_path) as f:
+                classes = yaml.safe_load(f)
+        except Exception as e:
+            logger.exception(f"Invalid labels file ['{labels_path}']\n\n{e}")
+            return
+        else:
+            classes = classes.get(next(iter(classes)), {})
+    logger.info(f"Labeled data will be saved to: {os.path.abspath(output_name)}")
+    annotate(images_path, output_name, classes, data_transfer, dst_folder, window_size, show_class_names, loop)
 
 def main() -> None:
     cli(prog_name='moevat')
